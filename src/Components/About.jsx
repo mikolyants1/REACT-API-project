c3778882f2df28eceb4a4733d13603d9fed827f9
mikolyants1 <y.mikolyants@mail.ru>
2023-08-05 12:12:09 +0300

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,21 +1,17 @@
-import {Component} from 'react'
-import {useParams,Link} from 'react-router-dom'
+import {useEffect,useState} from 'react'
+import axios from 'axios'
+import {useParams,Link,} from 'react-router-dom'
 import { Character,Types,Series } from '../styles/style.jsx'
-class Hero extends Component {
-    constructor(props){
-      super(props)
-      this.state={
-          json:null
-            }
-     }
-componentDidMount(){
-const params=this.props.params
-fetch(`https://rickandmortyapi.com/api/character/${params.id}`)
-.then(res=>res.json())
-.then((data)=>this.setState({json:data}))
+export default function About(){
+const params=useParams()
+const [json,setJson]=useState(null)  
+async function Fetch(){
+ return await axios.get(`https://rickandmortyapi.com/api/character/${params.id}`)
+ .then(({data})=>setJson(data))   
 }
-render(){
-const json=this.state.json
+useEffect(()=>{
+Fetch()
+},[])
 if (!json) return <div>loading...</div>
 return <Character>
 <img  src={json.image} alt="" />
@@ -28,16 +24,14 @@ return <Character>
 <div><span>Episodes:</span></div>
 <Series>
     {json.episode.map((item,index)=>{
-    return <div key={index}>
+    return <div className='episode' key={index}>
     <Link to={`${item.split('/')[5]}`}>episode {item.split('/')[5]},</Link>   
     </div>
 })}
 </Series>
-   <div>
+<div>
     <Link to='/'>Main</Link>
-  </div>
+    </div>
 </Character>
+
 }
-}
-const About=()=><Hero params={useParams()} />
-export default About
