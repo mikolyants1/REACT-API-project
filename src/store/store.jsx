085cb36e6fd2cf8f576f40a3a3491bb973672b19
mikolyants1 <y.mikolyants@mail.ru>
2023-08-09 10:59:55 +0300

    Update store.jsx

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,13 +1,12 @@
 import {create} from 'zustand'
 import {immer} from 'zustand/middleware/immer'
-import {devtools} from 'zustand/middleware'
-import {persist} from 'zustand/middleware'
+import {devtools,persist} from 'zustand/middleware'
 export const useStore=create(persist(devtools(immer((set)=>({
 page:1,
 nextPage:(index)=>set(state=>{
- state.page=index==7?7:index+1
+ state.page=index+1
 }),
 prevPage:(index)=>set(state=>{
- state.page=index==1?1:index-1
+ state.page=index-1
 })
 }))),{name:'store1',version:1}))
